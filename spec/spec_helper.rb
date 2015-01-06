04ec3c2f272f0821a694d9ce1e9551e151c8744f
holyshared <holy.shared.design@gmail.com>
2015-01-06 23:41:39 +0900

    add filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,10 @@ if ENV["COVERALLS_REPO_TOKEN"]
   Coveralls.wear!
 end
 
+SimpleCov.start do
+  add_filter do |source_file|
+    source_file.filename =~ /spec/ && !(source_file.filename =~ /fixtures/)
+  end
+end
+
 require 'simplecov-markdown'
