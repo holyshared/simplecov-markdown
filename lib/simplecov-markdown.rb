e6b67d01eaddab9c5571e2ac2624005d2781336c
Noritaka Horio <holy.shared.design@gmail.com>
2015-01-06 09:14:11 +0000

    add gem sources

---
+++ lib/simplecov-markdown.rb
@@ -0,0 +1,6 @@
+require "simplecov-markdown/version"
+
+class SimpleCov::Formatter::MarkdownFormatter
+  def format(result)
+  end
+end
---
+++ lib/simplecov-markdown/version.rb
@@ -0,0 +1,8 @@
+module SimpleCov
+  module Formatter
+    class MarkdownFormatter
+      VERSION = "0.1.0"
+    end
+  end
+end
+
