aabbb113631ef33bbbf2bdc879a815a25a669a2c
holyshared <holy.shared.design@gmail.com>
2015-01-06 23:30:22 +0900

    fix position

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
-require 'simplecov-markdown'
 
 if ENV["COVERALLS_REPO_TOKEN"]
   require 'coveralls'
   Coveralls.wear!
 end
+
+require 'simplecov-markdown'
