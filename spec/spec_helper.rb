b31d74e76642722c150b983ee1a3b3f2e314df2e
Noritaka Horio <holy.shared.design@gmail.com>
2015-01-09 02:36:24 +0000

    add console formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
+require 'simplecov-console'
 
 if ENV["COVERALLS_REPO_TOKEN"]
   require 'coveralls'
   Coveralls.wear!
+else
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::Console,
+    SimpleCov::Formatter::MarkdownFormatter
+  ]
 end
 
 SimpleCov.start do
